c63186668a545350a117c62ccac93c9cf2562ede
Muhammad Tayyab <110992545+M-Tayyab-Mustafa@users.noreply.github.com>
2025-08-19 15:04:23 +0500

    Update assertion for tokenization parameters
    The assertion now checks that tokenizationSpecificationParameters is not null when tokenizationSpecificationType is set to paymentGateway, ensuring required parameters are provided.

--- lib/src/google_pay.dart
+++ lib/src/google_pay.dart
@@ -109,7 +109,7 @@ class GooglePayButton extends StatefulWidget {
     required this.merchantName,
     this.child,
   }) : assert(
-         (tokenizationSpecificationType != TokenizationSpecificationType.paymentGateway) && (tokenizationSpecificationParameters != null),
+         !(tokenizationSpecificationType == TokenizationSpecificationType.paymentGateway && tokenizationSpecificationParameters == null),
          'Invalid tokenization specification: tokenizationSpecificationParameters are required when type is set to paymentGateway',
        );
 
